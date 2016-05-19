7d8805344bb0d0bae867009d4f10e9364504d75d
William Bezuidenhout <william.bezuidenhout+github@gmail.com>
2016-05-19 20:16:22 +0200

    Moved env vars loading. Simplyfied output writing

--- mail.py
+++ mail.py
@@ -1,10 +1,8 @@
 import requests, os
 
-__MAILGUN_BASE_URL = ""
-__MAILGUN_SECRET = ""
-__FROM_ADDRESS = ""
 
 def mail(dest, content):
+    __FROM_ADDRESS, __MAILGUN_BASE_URL, __MAILGUN_SECRET = load_env_vars()
     return requests.post(
         __MAILGUN_BASE_URL+"/messages",
         auth=("api",__MAILGUN_SECRET),
@@ -22,7 +20,6 @@ def load_recipients(loc):
     return recipients
 
 def load_mail_content(loc):
-    mail = open(loc)
     with open(loc) as mail_content:
         return mail_content.read()
 
@@ -30,25 +27,24 @@ def load_env_vars():
     __FROM_ADDRESS = os.getenv("FROM_ADDRESS")
     __MAILGUN_BASE_URL = os.getenv("MAILGUN_BASE_URL")
     __MAILGUN_SECRET = os.getenv("MAILGUN_SECRET")
-    if __FROM_ADDRESS is None:
+    if __FROM_ADDRESS is None or  __FROM_ADDRESS == "":
         raise Exception("Environment variable FROM_ADDRESS cannot be empty")
-    if __MAILGUN_BASE_URL is None:
+    if __MAILGUN_BASE_URL is None or __MAILGUN_BASE_URL == "":
         raise Exception("Environment variable MAILGUN_BASE_URL cannot be empty")
-    if __MAILGUN_SECRET is None:
+    if __MAILGUN_SECRET is None or __MAILGUN_SECRET == "":
         raise Exception("Environment variable MAILGUN_SECRET cannot be empty")
+    return __FROM_ADDRESS, __MAILGUN_BASE_URL, __MAILGUN_SECRET
 
 def main():
-    load_env_vars()
     recipients = load_recipients("recipients.txt")
     mail_content = load_mail_content("mail.html")
-    output = open("mail_result.txt", 'w')
-    for dest in recipients:
-        result = mail(dest, mail_content)
-        if result.status_code == 200:
-            output.write("{} : {}\n".format(dest, result.json()))
-        else:
-            print("Mailgun reported an error sending mail to {} -> {}".format(dest, result.text))
-    output.close()
+    with open("mail_result.txt", 'w') as output:
+        for dest in recipients:
+            result = mail(dest, mail_content)
+            if result.status_code == 200:
+                output.write("{} : {}\n".format(dest, result.json()))
+            else:
+                print("Mailgun reported an error sending mail to {} -> {}".format(dest, result.text))
 
 if __name__ == "__main__":
     main()
